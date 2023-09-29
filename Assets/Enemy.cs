04e85698a0fff57bfcf0f5fd5d3c6cc354478c35
artur93ever <felipe.93ramos@gmail.com>
2023-09-29 16:02:10 -0600

    Enemy HealthBar

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,23 +5,31 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Material whiteMaterial; // Assign your white material in the inspector
+
+    private Renderer myRenderer;
+    private Material originalMaterial;
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        myRenderer = GetComponent<Renderer>();
+        originalMaterial = myRenderer.material;
     }
 
     void OnTriggerEnter(Collider collision)
     {
-        if(collision.gameObject.tag == "Gloves")
+        if (collision.gameObject.tag == "Gloves")
         {
-            UnityEngine.Debug.Log("Enemy Hit");
+            StartCoroutine(ChangeColorCoroutine());
         }
+    }
+
+    IEnumerator ChangeColorCoroutine()
+    {
+        myRenderer.material = whiteMaterial; // Change to white material
+
+        yield return new WaitForSeconds(0.3f); // Wait for 0.3 seconds
+
+        myRenderer.material = originalMaterial; // Revert to original material
     }
 }
--- Assets/Health.cs
+++ Assets/Health.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public float maxHealth = 100f;
     public float currentHealth;
     public float deathTime;
+    public Slider healthSlider; // Reference to the health slider
+    public GameObject healthBar; // Reference to the health bar object
 
     void Start()
     {
         currentHealth = maxHealth;
+        healthSlider.maxValue = maxHealth; // Set the max value of the slider to maxHealth
+        healthSlider.value = currentHealth; // Set the current value of the slider to currentHealth
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+        healthSlider.value = currentHealth; // Update the slider value
         if (currentHealth <= 0)
         {
             Die();
@@ -25,6 +31,7 @@ public class Health : MonoBehaviour
     void Die()
     {
         // Perform death-related actions here
+        healthBar.SetActive(false); // or Destroy(healthBar);
         Invoke("DestroyObject", deathTime); // Destroy the object after 2 seconds
     }
 
